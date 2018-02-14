50047adf5f6da4aaa643278f6604de22e4f570cb
yy2735 <yy2735@columbia.edu>
2018-02-14 18:46:03 -0500

    0214

---
+++ A2-2/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(1800, 700);}
+
+function draw() {
+	background('pink');
+    fill(0,200,200);
+    rect(100,40,100,40);
+    ellipse(300,300,300,300);
+    textSize(40);
+    fill(0,200,200);
+    text('Yidi',150, 150);
+}
