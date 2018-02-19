d116cc26df70a7542b96cb5de236066fe9dd7032
yy2735 <yy2735@columbia.edu>
2018-02-19 04:03:19 -0500

    4

--- A2-3/sketch.js
+++ A2-3/sketch.js
@@ -9,28 +9,18 @@ function draw(){
   noStroke();
   background(100);
 
-  // Put time in variables for easy testing
   
 var s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
   var m = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
   var h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
-  // Testing min state
-  // var s = 0;
-  // var m = 0;
-  // var h = 0;
-  // Testing max state
-  // var s = 60;
-  // var m = 60;
-  // var h = 24;
 
-    fill(255,255,0,150) // blue
+    fill(255,255,0,150) 
  arc(x,y,400,400,0,h,PIE)
 
-  fill(0,0,255,150) // yellow
+  fill(0,0,255,150) 
  arc(x,y,300,300,0,m,OPEN)
 
-  fill(255,0,0,150) // red
+  fill(255,0,0,150)
   arc(x,y,200,200,0,s,CHORD);
-
 
 }
