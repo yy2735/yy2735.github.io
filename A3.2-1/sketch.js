c9c0806b6fe1043fa06937766236ee99abc54f0f
yy2735 <yy2735@columbia.edu>
2018-03-31 21:35:34 -0400

    s

--- A3.2-1/sketch.js
+++ A3.2-1/sketch.js
@@ -1,14 +1,10 @@
-//Question :: How to load font ? .ttf
-
 var headlines = [];
-var section = [];
-var subsection = [];
-var maxHeadLen, minHeadLen;
 
 function preload() {
+
   // Assemble url for API call
   var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-  var apikey = "e0b773a6804541fcb64d23e7c14bc2fd"; // see: https://developer.nytimes.com
+  var apikey = "4c76a36ff8bf459b98a064a3a0e9299b"; // see: https://developer.nytimes.com
   url += "?api-key=" + apikey;
 
   nytResponse = loadJSON(url);
@@ -17,35 +13,26 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(750, 800);
-  background(255);
+  createCanvas(500, 1000);
+  background(0);
 
   textSize(10);
   textAlign(LEFT);
 
-  //noLoop(); // since we're not animating, one frame is sufficient: run draw() just once
+  noLoop(); 
 
   extractHeadlines();
+  extractSections();
 }
 
 function draw() {
-  background(255);
+  background(0);
 
-  // Set the left and top margin
-  var margin = 40;
+  var lineheight = 50;
+  var margin = 10;
   translate(margin, margin);
 
-  var lineheight = 30;
-  var rectheight = 10;
-
-  //title of the page
-  fill(0);
-  textSize(50)
-  textStyle(BOLD);
-  textFont('Georgia');
-  text("New York Times", 0, 30);
-
-  for (var i = 0; i < headlines.length; i++) {
+   for (var i = 0; i < headlines.length; i++) {
     //draw rectangle
     fill(191,227,255,150);
     rect(0, 100 + i*lineheight, 6*headlines[i].length)
