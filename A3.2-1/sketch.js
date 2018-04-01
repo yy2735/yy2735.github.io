f96487899758c4c543cc34a3699ad4fab78fa44c
yy2735 <yy2735@columbia.edu>
2018-04-01 01:04:08 -0400

    x

--- A3.2-1/sketch.js
+++ A3.2-1/sketch.js
@@ -16,7 +16,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1000,1000);
+  createCanvas(500,1000);
   background(0);
 
   textSize(10);
@@ -31,25 +31,29 @@ function draw() {
 
  
   var margin = 10;
-  var lineheight = 30;
+  var lineheight = 10;
   var rectheight = 10;
   translate(margin, margin);
 
 
 
   for (var i = 0; i < headlines.length; i++) {
+    //draw rectangle
+    fill(191,227,255,150);
+    rect(0, 100 + i*lineheight, 6*headlines[i].length)
 
-    fill("#ff9900");
-    
+    //draw headline, section, subsection
 
+    // fill(0, 128 + sin(frameCount*0.1) * 128, 0);
+    // text(section[i], 540, 100 + i*lineheight);
     if (mouseX > margin && mouseX < width - margin && mouseY < 120 + margin+i*lineheight && mouseY > 80 + margin+i*lineheight+(-1*rectheight)) {
       push();
-      fill("#99cc00");
+      fill("#FFC859");
       textStyle(BOLD);
       textFont('Helvetica');
-      textSize(10 + (mouseY / margin)*2);
+      textSize(10 + (mouseX / width)*30);
       textAlign(RIGHT);
-      text(headlines[i], 100, 100 + i*lineheight); 
+      text(headlines[i], 800, 100 + i*lineheight); 
       pop();
     } else {
       fill("#00A6FF")
@@ -80,12 +84,10 @@ function extractHeadlines() {
       minHeadLen = h.length;
     }
     append(headlines, h);
-<<<<<<< HEAD
-  }
-=======
     //console.log(ss);
   }
 
-
+  //console.log(headlines); // make sure counted data looks as expected
+  //console.log(maxHeadLen);
+  //console.log(minHeadLen);
 }
-
