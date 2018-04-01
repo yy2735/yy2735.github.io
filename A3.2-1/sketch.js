d8a7f4af70df0c7be2d4c01596f4cdb444ca83b1
yy2735 <yy2735@columbia.edu>
2018-04-01 01:17:31 -0400

    s

--- A3.2-1/sketch.js
+++ A3.2-1/sketch.js
@@ -5,14 +5,12 @@ var maxHeadLen, minHeadLen;
 
 function preload() {
 
-  // Assemble url for API call
   var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
   var apikey = "4c76a36ff8bf459b98a064a3a0e9299b"; // see: https://developer.nytimes.com
   url += "?api-key=" + apikey;
 
   nytResponse = loadJSON(url);
-  // loadJSON() is asynchronous, but calling it inside preload() guarantees
-  // we'll have a response before setup() and draw() is run.
+
 }
 
 function setup() {
@@ -40,14 +38,10 @@ function draw() {
 
 
   for (var i = 0; i < headlines.length; i++) {
-    //draw rectangle
     fill("#3366ff");
-    rect(0, 100 + i*lineheight, 6*headlines[i].length)
+    rect(100, 100 + i*lineheight, 4*headlines[i].length)
 
-    //draw headline, section, subsection
 
-    // fill(0, 128 + sin(frameCount*0.1) * 128, 0);
-    // text(section[i], 540, 100 + i*lineheight);
     if (mouseX > margin && mouseX < width - margin && mouseY < 120 + margin+i*lineheight && mouseY > 80 + margin+i*lineheight+(-1*rectheight)) {
       push();
       fill("#99ff33");
@@ -58,7 +52,7 @@ function draw() {
       text(headlines[i], 50, 50 + i*lineheight); 
       pop();
     } else {
-      fill("#ff9900")
+      fill("#3366ff")
       textSize(12)
       textFont('Helvetica');
       text(headlines[i], 0, 100 + i*lineheight);    }
@@ -67,13 +61,11 @@ function draw() {
 }
 
 function extractHeadlines() {
-  //console.log(nytResponse); // take a look at the full API response structure
+  
 
   for (var i = 0; i < (nytResponse.results.length); i++) {
     var h = nytResponse.results[i].title;
-    // besides .title, other text data available to you include:
-    // .abstract, .byline, .section, etc. etc.
-
+   
     if (!maxHeadLen) {
       maxHeadLen = h.length;
     } else if (h.length > maxHeadLen) {
@@ -86,10 +78,7 @@ function extractHeadlines() {
       minHeadLen = h.length;
     }
     append(headlines, h);
-    //console.log(ss);
+    
   }
 
-  //console.log(headlines); // make sure counted data looks as expected
-  //console.log(maxHeadLen);
-  //console.log(minHeadLen);
 }
