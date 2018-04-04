b24fea4808a4e68ac42bfb0d2da0e389ba76338b
yy2735 <yy2735@columbia.edu>
2018-04-03 21:56:32 -0400

    f

--- A4.2/sketch.js
+++ A4.2/sketch.js
@@ -1,40 +1,19 @@
-// var table;
-// var borough;
-// var amount;
-
-// function preload(){
-//   table = loadTable('Public_Recycling_Bins_Borough.csv', 'csv', 'header');
-// }
 
 function setup() {
   createCanvas(1000, 1000);
-  // var borough = table.getColumn("Borough");
-  // console.log(borough);
+
 
 }
 
-// function loadData() {
-//   borough = table.getColumn("Borough");
-//   amount = table.getColumn("Amount");
-// }
 
-// function draw(){
+function draw(){
 
-//   // background(100, 100, 100, 200);
-//   // fill(255, 255, 255);
-//   // textSize(20);
-//   // text("Public Recycling Bins Count by Borough", 20,20);
-//   // var lineheight = 25;
-//   // var rectheight = 15;
-//   // textSize(12);
+  background(100, 100, 100, 200);
+  fill(255, 255, 255);
+  textSize(20);
+  text("Public Recycling Bins Count by Borough", 20,20);
+  var lineheight = 25;
+  var rectheight = 15;
+  textSize(12);
 
-//   // for (var i = 0; i < amount.length; i++) {
-//   //   fill(190, 30, 70);
-//   //   var rectwidth = map(amount[i], 0, 163.75, 0, 1000);
-//   //   rect(0, (i+2)*lineheight, rectwidth, -1*rectheight)
-
-//   //   fill(255, 255, 255);
-//   //   text(Borough[i], 0, (i+2)*lineheight);
-
-//   console.log(amount);
-// }
+}
