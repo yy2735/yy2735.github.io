d605fdb857495455e059664ed7084cb151242995
yy2735 <yy2735@columbia.edu>
2018-05-05 13:40:14 +0800

    op

--- A4.2/sketch_1.js
+++ A4.2/sketch_1.js
@@ -8,7 +8,6 @@ var state = 0;
 var button;
 var buttonText = "Explore by Bin Type";
 
-
 function preload(){
   table = loadTable('Public_Recycling_Bins_Borough.csv', 'csv', 'header');
   tableEthnicity = loadTable('Type.csv', 'csv');
@@ -22,7 +21,7 @@ function setup() {
   createCanvas(1200,500);
   loadData();
 
-  textSize(18);
+  textSize(25);
   noStroke();
 }
 
@@ -32,27 +31,12 @@ function loadData() {
   ethnicityTable = tableEthnicity.getArray();
 }
 
-function 
-draw(){
-rect(50, 350,10,10)
-fill(240,90,40)
-
-rect(50, 370,10,10)
-fill(225,110,60)
-
-rect(50, 390,10,10)
-fill(255,130,80)
-
-rect(50, 4100,10,10)
-fill(255,150,100)
-}
-
 function draw(){
   background(255,255,255);
   if (state === 0 ){
     fill("#86B956");
     textSize(30);
-    text("Public Recycling Bins Count by Borough",50,30);
+    text("Public Recycling Bins Count by Borough",100,40);
     var lineheight = 30;
     var rectheight = 20;
     textSize(12);
@@ -60,7 +44,7 @@ function draw(){
     for (var i = 0; i < amount.length; i++) {
       fill("#57AFCE");
       var rectwidth = map(amount[i], 0, 163.75, 0, 1000);
-      rect(100, (i+2)*lineheight, rectwidth, -1*rectheight)
+      rect(200, (i+2)*lineheight, rectwidth, -1*rectheight)
 
       fill("#86B956");
       text(borough[i], 0, (i+2)*lineheight);
@@ -69,18 +53,18 @@ function draw(){
   else {
     fill("#86B956");
     textSize(30);
-    text("Public Recycling Bins Count by Type",50,30);
+    text("Public Recycling Bins Count by Type",100,40);
     var lineheight = 30;
     var rectheight = 20;
     for (var i = 0; i < ethnicityTable.length; i++) {
       totalwidth=100;
        for (var j = 0; j < 5; j++){
-      R_colour=240+j*20;
-      G_colour=90+j*20;
-      B_colour=40+j*20;
+      R_colour=190+j*100;
+      G_colour=30+j*100;
+      B_colour=70+j*100;
       fill(R_colour,G_colour,B_colour);
-         }
-      
+
+      //rect (100, 100, 60, 50);
       var rectwidth = map(ethnicityTable[i][j], 0, 163.75, 0, 1000);
       rect(totalwidth, (i+2)*lineheight, rectwidth, -1*rectheight);
       totalwidth=totalwidth+rectwidth;
@@ -88,11 +72,34 @@ function draw(){
     }
 
     push();
-   
+    // Legend
+    // textSize(14);
+    // fill(210, 80, 150);
+    // rect(30, 40, 30, 30);
+    // fill(255, 255, 255);
+    // text("Top 10 Cause", 65, 60);
+    //
+    // fill(190, 30, 70);
+    // rect(170, 40, 30, 30);
+    // fill(255, 255, 255);
+    // text("Top 3 Cause", 205, 60);
+
+    // textSize(18);
+    // // Rows of grid: ethnicity
+    // fill(255, 255, 255);
+    // text("Asian",25,120);
+    // text("Black",25,165);
+    // text("Hispanic",5,210);
+    // text("White",25,255);
+
+    // Columns of grid: leading causes
+    // rotate(PI / 2.0);
+    // for (var i = 0; i < leadingCauses.length; i++) {
+    //   text(leadingCauses[i], 280, -100-(i*45));
+    // }
     pop();
   }
 }
-
 
 function toggleState() {
   state = (state == 0) ? 1 : 0;
---
+++ A7/project.js
@@ -0,0 +1,172 @@
+
+
+ var table;
+ var la=[];
+ var long=[];
+
+
+
+
+function preload() {
+
+  table = loadTable('project.csv', 'csv', 'header');
+  console.log(table);
+
+
+}
+
+function setup() {
+  width = 2000;
+  length = 2000;
+  createCanvas(width, length);
+  background(0);
+  latitude = table.getColumn("a");
+  longtitude = table.getColumn("b");
+  type = table.getColumn("Site_type");
+  console.log(latitude);
+  console.log(longtitude);
+  for (var i = 0; i < latitude.length; i++) {
+    var l = Number(latitude[i]);
+    var m = Number(longtitude[i]);
+    append(la, l);
+    append(long, m);
+
+  }
+  console.log(la);
+  console.log(long);
+  ra = 6;
+  angleMode(DEGREES);
+
+  var button = createButton("Show Bin Type");
+  button.mousePressed(showType);
+
+  var lafirst = max(la);
+  var lasecond = max(la);
+
+ for (var i = 0; i < 1000; i++) {
+  var h = la[i];
+  if ( h < lafirst )
+  { lasecond = lafirst;
+    lafirst = h;
+
+ }
+
+}
+
+ var longfirst = min(long);
+  var longsecond = min(long);
+
+ for (var i = 0; i < 1000; i++) {
+  var h = long[i];
+  if ( h > longfirst )
+  { longsecond = longfirst;
+    longfirst = h;
+
+ }
+}
+
+
+
+
+for (var i = 0; i < 1000; i++) {
+  var h = map(la[i], lasecond, max(la), 0, length);
+  var k = map(long[i], min(long), longsecond, 0, width);
+
+
+  ellipse(k, length-h, ra, ra);
+
+
+
+
+ }
+
+
+}
+
+function showType() {
+  var lafirst = max(la);
+  var lasecond = max(la);
+
+ for (var i = 0; i < 1000; i++) {
+  var h = la[i];
+  if ( h < lafirst )
+  { lasecond = lafirst;
+    lafirst = h;
+
+ }
+
+}
+
+ var longfirst = min(long);
+  var longsecond = min(long);
+
+ for (var i = 0; i < 1000; i++) {
+  var h = long[i];
+  if ( h > longfirst )
+  { longsecond = longfirst;
+    longfirst = h;
+
+ }
+}
+
+
+
+
+for (var i = 0; i < 1000; i++) {
+
+  var h = map(la[i], lasecond, max(la), 0, length);
+  var k = map(long[i], min(long), longsecond, 0, width);
+
+  if (type[i]==="Subproperty") {
+    fill(100,30,170);
+    ellipse(k, length-h, ra, ra);
+  }
+  //ellipse(k, length-h, ra, ra);
+  if (type[i]==="Indoor") {
+    fill("#86B956");
+    ellipse(k, length-h, ra, ra);
+  }
+
+  if (type[i]==="Outdoor") {
+    fill("#57AFCE");
+    ellipse(k, length-h, ra, ra);
+  }
+
+  if (type[i]==="Greenthumb") {
+    fill(255, 255, 255);
+    ellipse(k, length-h, ra, ra);
+  }
+
+
+
+ }
+
+}
+
+
+
+
+
+function draw() {
+noStroke();
+fill(255);
+textSize(15);
+text('NYC Public Recycling Bins Type', width-500, length-50);
+fill(100,30,170);
+ellipse(width-500, length-80, ra*2, ra*2);
+text('Subproperty', width-500+10, length-80+ra);
+fill("#86B956");
+ellipse(width-400, length-80, ra*2, ra*2);
+text('Indoor', width-400+10, length-80+ra);
+fill("#57AFCE");
+ellipse(width-300, length-80, ra*2, ra*2);
+text('Outdoor', width-300+10, length-80+ra);
+fill(255, 255, 255);
+ellipse(width-200, length-80, ra*2, ra*2);
+text('Greenthumb', width-200+10, length-80+ra);
+
+
+
+
+
+}
